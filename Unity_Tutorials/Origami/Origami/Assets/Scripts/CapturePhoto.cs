3e49168954c1bc2837a67f07898c61638f5d847c
David Gitau <d.chege711@gmail.com>
2017-07-06 16:44:28 -0400

    Started Holograms 101 Tutorial

---
+++ Unity_Tutorials/Origami/Origami/Assets/Scripts/CapturePhoto.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR.WSA.WebCam;
+using System.Linq;
+
+// Code obtained from https://developer.microsoft.com/en-us/windows/mixed-reality/locatable_camera_in_unity
+
+public class CapturePhoto : MonoBehaviour {
+
+	// Start by creating a PhotoCapture Object
+	PhotoCapture photoCaptureObject = null;
+	void Start () {
+		PhotoCapture.CreateAsync (false, OnPhotoCaptureCreated);
+	}
+
+	// Next, store our object, set our parameters, and start Photo Mode
+	void OnPhotoCaptureCreated(PhotoCapture captureObject) {
+		
+		photoCaptureObject = captureObject;
+
+		Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+		CameraParameters c = new CameraParameters();
+		c.hologramOpacity = 0.0f;
+		c.cameraResolutionWidth = cameraResolution.width;
+		c.cameraResolutionHeight = cameraResolution.height;
+		c.pixelFormat = CapturePixelFormat.BGRA32;
+
+		captureObject.StartPhotoModeAsync(c, OnPhotoModeStarted);
+	}
+
+	// We want to capture the photo directly to a PNG file and store it on disk
+	// I'll later upload the photos to Github for clearer documentation
+	private void OnPhotoModeStarted(PhotoCapture.PhotoCaptureResult result)
+	{
+		if (result.success)
+		{
+			string filename = string.Format(@"CapturedImage{0}_n.jpg", Time.time);
+			string filePath = System.IO.Path.Combine(Application.persistentDataPath, filename);
+
+			photoCaptureObject.TakePhotoAsync(filePath, PhotoCaptureFileOutputFormat.PNG, OnCapturedPhotoToDisk);
+			Debug.Log (filePath);
+		}
+		else
+		{
+			Debug.LogError("Unable to start photo mode!");
+		}
+	}
+
+	// After capturing photo to disk, exit photo mode and clean up our objects
+	void OnCapturedPhotoToDisk(PhotoCapture.PhotoCaptureResult result)
+	{
+		if (result.success)
+		{
+			Debug.Log("Saved Photo to disk!");
+			photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
+		}
+		else
+		{
+			Debug.Log("Failed to save Photo to disk");
+		}
+	}
+
+
+	// Clean up once you're done. Preserves resources.
+	void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+	{
+		photoCaptureObject.Dispose();
+		photoCaptureObject = null;
+	}
+	
+	// Update is called once per frame
+	// void Update () {
+	//	
+	// }
+}
--- Unity_Tutorials/Unity_Tutorials_README.md
+++ Unity_Tutorials/Unity_Tutorials_README.md
@@ -1,6 +1,20 @@
 # Unity Tutorials README
 * To set up the development environment, follow [these instructions](https://developer.microsoft.com/en-us/windows/mixed-reality/install_the_tools).
 
+----
+
 ## [Roll a Ball](https://unity3d.com/learn/tutorials/projects/roll-ball-tutorial/introduction-roll-ball?playlist=17141)
 * I followed the tutorial linked in the title.
 * Most of the development was done inside Unity, but in the Scripts folder, you can find some C# code.
+
+----
+
+## Notes on [Holograms 101](https://developer.microsoft.com/en-us/windows/mixed-reality/holograms_101)
+
+### Chapter 1 - 'Holo' World
+* The Main Camera for HoloApps has a pattern to it:
+	* Transform Position = (0, 0, 0); Clear Flags = Solid color; Background RGBA = (0, 0, 0, 0) since black == transparent for HoloLens
+* The scene:
+	* There was a lot of handwaving in this part of the tutorial, since I merely dragged and dropped already-made GameObjects
+	* There's much to learn about how to scale/position the holograms, change the lighting, etc.
+* I'm getting compilation errors when trying to export to Visual Studio, so I'll use the HoloLens directly from Unity.
