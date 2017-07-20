4ae60546274010cf035d94064a01095aa23ec8a7
dchege711 <d.chege711@gmail.com>
2017-07-20 17:18:58 -0400

    Finished comparing Wireshark to WDP Data.

--- Quantitative_Research/ProcessDataDumps.py
+++ Quantitative_Research/ProcessDataDumps.py
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
+import re
 
 #_______________________________________________________________________________
 
@@ -30,51 +31,77 @@ def getWDPStats(wdpDump):
     07/19/2017-16:02:12.9595648,Microsoft-Windows-Kernel-Network,10,Keyword:9223372036854776000,Level:4,Message:TCPv4: 143 bytes transmitted from 10.8.113.245:443 to 10.8.227.69:55939. ,PID:4,ProviderName:Microsoft-Windows-Kernel-Network,TaskName:KERNEL_NETWORK_TASK_TCPIP,connid:0,daddr:10.8.227.69,dport:55939,endtime:931204,saddr:10.8.113.245,seqnum:0,size:143,sport:443,startime:931204,WebbCompletePayload:message:tcpv4: 143 bytes transmitted from 10.8.113.245:443 to 10.8.227.69:55939. , pid:4, connid:0, daddr:10.8.227.69, dport:55939, endtime:931204, saddr:10.8.113.245, seqnum:0, size:143, sport:443, startime:931204,
     ...
     '''
+    # The time stamps are pretty dense (numerous points per second)
+    # We want to group the data using seconds as our resolution
+    timeRegEx = r"\d{2}\/\d{2}\/\d{4}-\d{2}:\d{2}:\d{2}"
+    laptopAsDestRegEx = r"daddr:10\.8\.227\.69"
+    laptopAsSenderRegEx = r"saddr:10\.8\.227\.69"
+    sizeRegEx = r"size:\d{0,}"
 
     # Initialize relevant variables
     holoLensToLaptop_timeStamps = []
-    holoLensToLaptop_bytes = []
+    holoLensToLaptop_MB = []
     laptopToHoloLens_timeStamps = []
-    laptopToHoloLens_bytes = []
+    laptopToHoloLens_MB = []
+
+    sent = 0
+    received = 0
 
     with open(wdpDump) as myData:
-        headerInfo = myData.readline()
+        # We don't need the header information
+        myData.readline()
 
+        # Initialize helper variables
+        prevTimeStampSent = "07/19/2017-16:02:12"   #
+        prevTimeStampReceived = "07/19/2017-16:02:12"
+        runningDataSumSent = 0
+        runningDataSumReceived = 0
+
+        # The data is not uniformly formatted, hence regular expressions
         for line in myData:
-            items = line.split(',')
-            timeStamp = items[0]
 
-            # We tested if the position of the message and its format is consistent
-            # 'Message...' is always on index 5, but the content is not uniform
-            # Instead of multiple if-statements, we'll do entire scans of the line
+            # Search for the relevant data points
+            timeStamp = re.search(timeRegEx, line)[0] # Gets the text itself
+            sendingToLaptop = re.search(laptopAsDestRegEx, line)
+            receivingFromLaptop = re.search(laptopAsSenderRegEx, line)
+            dataPresent = re.search(sizeRegEx, line)
 
-            # Look for the relevant strings
-            for i in range(len(items)):
-                labelAndItem = items[i].split(':')
-                # Extract the destination address
-                if labelAndItem[0] == 'daddr':
-                    destAddress = labelAndItem[1]
-                # Extract the sending address
-                if labelAndItem[0] == 'saddr':
-                    senderAddress = labelAndItem[1]
-                # Extract the number of bytes transferred
-                if labelAndItem[0] == 'size':
-                    numberOfBytes = labelAndItem[1]
+            if sendingToLaptop and dataPresent:
+                currentBytes = int(dataPresent[0].split(':')[1])
+                # Increment the total if we're still in the same second
+                if timeStamp == prevTimeStampSent:
+                    runningDataSumSent += currentBytes
+                # Append the total and reset sum if we've moved to a new sec
+                else:
+                    holoLensToLaptop_timeStamps.append(prevTimeStampSent)
+                    holoLensToLaptop_MB.append(runningDataSumSent / (1024 * 1024))
+                    sent += runningDataSumSent / (1024 * 1024)
+                    prevTimeStampSent = timeStamp
+                    runningDataSumSent = currentBytes
 
-            # Update the holoLensToLaptop list
-            if destAddress == laptopIPv4:
-                # holoLensToLaptop.append((timeStamp, numberOfBytes))
-                holoLensToLaptop_timeStamps.append(timeStamp)
-                holoLensToLaptop_bytes.append(numberOfBytes)
-            # Update the laptopToHoloLens list
-            elif senderAddress == laptopIPv4:
-                # laptopToHoloLens.append((timeStamp, numberOfBytes))
-                laptopToHoloLens_timeStamps.append(timeStamp)
-                laptopToHoloLens_bytes.append(numberOfBytes)
+            if receivingFromLaptop and dataPresent:
+                currentBytes = int(dataPresent[0].split(':')[1])
+                # Increment the total if we're still in the same second
+                if timeStamp == prevTimeStampReceived:
+                    runningDataSumReceived += currentBytes
+                # Append the total and reset sum if we've moved to a new sec
+                else:
+                    laptopToHoloLens_timeStamps.append(prevTimeStampReceived)
+                    laptopToHoloLens_MB.append(runningDataSumReceived / (1024 * 1024))
+                    received += runningDataSumReceived / (1024 * 1024)
+                    prevTimeStampReceived = timeStamp
+                    runningDataSumReceived = currentBytes
 
-    # Communicate status to terminal and return the results
-    # print(str(len(holoLensToLaptop)), "to laptop,", str(len(laptopToHoloLens)), "from laptop")
-    return (holoLensToLaptop_timeStamps, holoLensToLaptop_bytes, laptopToHoloLens_timeStamps, laptopToHoloLens_bytes)
+    # Communicate status to terminal
+    print(  "Sent {0:.2f} MB".format(sent),
+            ", received {0:.2f} MB".format(received),
+            ", total = {0:.2f} MB".format(sent+received)
+    )
+    print("\nSent Array Length", str(len(holoLensToLaptop_timeStamps)), "/", str(len(holoLensToLaptop_MB)))
+    print("\nReceived Array Length", str(len(laptopToHoloLens_timeStamps)), "/", str(len(laptopToHoloLens_MB)))
+
+    # Return results
+    return (holoLensToLaptop_timeStamps, holoLensToLaptop_MB, laptopToHoloLens_timeStamps, laptopToHoloLens_MB)
 
 #_______________________________________________________________________________
 
@@ -95,44 +122,69 @@ def getWiresharkStats(wiresharkDump):
     "18","2017-07-19 16:02:12.801502","10.8.113.245","10.8.227.69","TCP","54","443  >  55939 [ACK] Seq=226 Ack=477 Win=258 Len=0"
     ...
     '''
-    # Indexes for accessing the Wireshark CSV data
-    NUMBER = 0
-    TIME = 1
-    SOURCE = 2
-    DESTINATION = 3
-    PROTOCOL = 4
-    LENGTH = 5
-    INFO = 6
-
     # Initialize relevant variables
     holoLensToLaptop_timeStamps = []
-    holoLensToLaptop_packets = []
+    holoLensToLaptop_megapackets = []
     laptopToHoloLens_timeStamps = []
-    laptopToHoloLens_packets = []
+    laptopToHoloLens_megapackets = []
+
+    sent = 0
+    received = 0
 
     with open(wiresharkDump) as myData:
         headerInfo = myData.readline()
 
+        # Initialize helper variables
+        prevTimeStampSent = "2017-07-19 16:02:02"
+        prevTimeStampReceived = "2017-07-19 16:02:02"
+        runningDataSumSent = 0
+        runningDataSumReceived = 0
+        TIME = 1
+        SOURCE = 2
+        DESTINATION = 3
+        LENGTH = 5
+
         for line in myData:
             line = line.replace('"', '')    # Urgh, why did Wireshark do this?
             items = line.split(",")
-            timeStamp = items[TIME]
-            if items[SOURCE] == holoLensIPv4:
-                # holoLensToLaptop.append((timeStamp, items[LENGTH]))
-                holoLensToLaptop_timeStamps.append(timeStamp)
-                holoLensToLaptop_packets.append(items[LENGTH])
-            elif items[DESTINATION] == holoLensIPv4:
-                # laptopToHoloLens.append((timeStamp, items[LENGTH]))
-                laptopToHoloLens_timeStamps.append(timeStamp)
-                laptopToHoloLens_packets.append(items[LENGTH])
+            timeStamp = items[TIME].split('.')[0]
+            megaPackets = float(items[LENGTH]) / 1000000
 
-    # Communicate status to terminal and return the results
-    # print(str(len(holoLensToLaptop)), "to laptop,", str(len(laptopToHoloLens)), "from laptop")
-    return (holoLensToLaptop_timeStamps, holoLensToLaptop_packets, laptopToHoloLens_timeStamps, laptopToHoloLens_packets)
+            if items[SOURCE] == holoLensIPv4:
+                sent += megaPackets
+                if timeStamp == prevTimeStampSent:
+                    runningDataSumSent += megaPackets
+                else:
+                    holoLensToLaptop_timeStamps.append(prevTimeStampSent)
+                    holoLensToLaptop_megapackets.append(runningDataSumSent)
+                    prevTimeStampSent = timeStamp
+                    runningDataSumSent = megaPackets
+
+
+            elif items[DESTINATION] == holoLensIPv4:
+
+                received += megaPackets
+                if timeStamp == prevTimeStampReceived:
+                    runningDataSumReceived += megaPackets
+                else:
+                    laptopToHoloLens_timeStamps.append(prevTimeStampReceived)
+                    laptopToHoloLens_megapackets.append(runningDataSumReceived)
+                    prevTimeStampReceived = timeStamp
+                    runningDataSumReceived = megaPackets
+
+    # Communicate status to terminal
+    print(  "Sent {0:.2f} megapackets".format(sent),
+            ", received {0:.2f} megapackets".format(received),
+            ", total = {0:.2f} megapackets".format((sent+received))
+    )
+    print("\nSent Array Length", str(len(holoLensToLaptop_timeStamps)), "/", str(len(holoLensToLaptop_megapackets)))
+    print("\nReceived Array Length", str(len(laptopToHoloLens_timeStamps)), "/", str(len(laptopToHoloLens_megapackets)))
+
+    return (holoLensToLaptop_timeStamps, holoLensToLaptop_megapackets, laptopToHoloLens_timeStamps, laptopToHoloLens_megapackets)
 
 #_______________________________________________________________________________
 
-def compareDataOnGraph(title, datasetOneTime, dataSetOneData, datasetTwoTime, datasetTwoData):
+def compareDataOnGraph(titleAndLabels, datasetOneTime, dataSetOneData, datasetTwoTime, datasetTwoData):
 
     # timeOne, valuesDataSetOne = np.loadtxt(dataSetOne, unpack = True,
     #                             converters = )
@@ -149,11 +201,13 @@ def compareDataOnGraph(title, datasetOneTime, dataSetOneData, datasetTwoTime, da
     timeTwo = pd.to_datetime(datasetTwoTime)
 
     plt.figure(1)
-    plt.plot_date(x = timeOne, y = dataSetOneData, fmt = 'r:')
-    plt.show()
-
-    plt.figure(2)
-    plt.plot_date(x = timeTwo, y = datasetTwoData, fmt = 'b:')
+    plt.grid(True)
+    plt.xlabel('Time')
+    plt.ylabel('Rate of Data Transfer (Megapackets))')
+    plt.title(titleAndLabels[0])
+    plt.plot_date(x = timeOne, y = dataSetOneData, fmt = 'r-', label = titleAndLabels[1])
+    plt.plot_date(x = timeTwo, y = datasetTwoData, fmt = 'b-', label = titleAndLabels[2])
+    plt.legend(loc = 'best')
     plt.show()
 
 #_______________________________________________________________________________
@@ -161,7 +215,11 @@ def compareDataOnGraph(title, datasetOneTime, dataSetOneData, datasetTwoTime, da
 def main():
     wdpStats = getWDPStats('Origami_Windows_Device_Portal.csv')
     wiresharkStats = getWiresharkStats('Origami_Wireshark.csv')
-    compareDataOnGraph("Nah!", wiresharkStats[0], wiresharkStats[1], wdpStats[0], wdpStats[1])
+    titleAndLabels = [  'Data Transfer by the Origami App',
+                        'HoloLens to Laptop',
+                        'Laptop to HoloLens'
+    ]
+    compareDataOnGraph(titleAndLabels, wiresharkStats[0], wiresharkStats[1], wiresharkStats[2], wiresharkStats[3])
 
 #_______________________________________________________________________________
 
--- Quantitative_Research/README.md
+++ Quantitative_Research/README.md
@@ -4,3 +4,4 @@
     * [01_Laptop-as-a-server_and_HoloLens-as-a-display](https://github.com/dchege711/Augmented_Reality/blob/master/Quantitative_Research/01_Laptop-as-a-server_and_HoloLens-as-a-display.md)
         * We ran a holographic app in my laptop, and connected to a HoloLens via Wi-Fi.
         * We then measured the rate of data transfer over the Wi-Fi network and graphed the results.
+        
