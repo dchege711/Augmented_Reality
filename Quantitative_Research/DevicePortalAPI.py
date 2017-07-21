1734ebe8f3d89ab7a09f37ecae752ec9bd3d1aa3
dchege711 <d.chege711@gmail.com>
2017-07-21 13:00:44 -0400

    Streamlined the logging operations of DevicePortalAPI.py. The data will be dumped in the Data_Dumps folder.

--- Quantitative_Research/DevicePortalAPI.py
+++ Quantitative_Research/DevicePortalAPI.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import sys
 import os
 from datetime import datetime as dt
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -11,11 +12,17 @@ holoLensIPv4 = os.environ['HL_CHEGE_IPV4']
 baseURL = "https://" + holoLensIPv4 + "/"
 USERNAME = os.environ['WDP_USERNAME']
 PASSWORD = os.environ['WDP_PASSWORD_HL_CHEGE']
+dtFormat = "%Y-%m-%d %H:%M:%S.%f"
+fileNameDTFormat = "%m-%d-%H_%M"
 
-def logPerformanceStats():
+def logPerformanceStats(outputFile):
     '''
-    Returns a dict of the performance stats
+    Prints out performance stats (tab-delimited) to the specified text file.
+    The stats are:
+        TimeStamp, CPULoad, DedicatedMemory, DedicatedMemoryUsed, SystemMemory,
+        SystemMemoryUsed, EnginesUtilization
 
+    Sample result from the GET request...
     {'Reason': ' {
         "AvailablePages" : 230370,
         "CommitLimit" : 764290,
@@ -53,8 +60,32 @@ def logPerformanceStats():
     # We've suppressed the warnings and ignored verifications
     r = requests.get(performanceURL, verify = False, auth = (USERNAME, PASSWORD))
     textAsDict = json.loads(r.json()['Reason'])
-    print(dt.now(), end = ", ")
-    print(textAsDict['CpuLoad'], textAsDict['NetworkingData']['NetworkOutBytes'])
+    gpuData = textAsDict['GPUData']['AvailableAdapters'][0]
+    outputFile.write(
+            dt.now().strftime(dtFormat) + "\t" +
+            str(textAsDict['CpuLoad']) + "\t" +
+            str(gpuData['DedicatedMemory']) + "\t" +
+            str(gpuData['DedicatedMemoryUsed']) + "\t" +
+            str(gpuData['SystemMemory']) + "\t" +
+            str(gpuData['SystemMemoryUsed']) + "\t" +
+            str(gpuData['EnginesUtilization']) + "\n"
+    )
 
-for i in range(3):
-    logPerformanceStats()
+
+def writePerformanceStats():
+    currentDir = os.path.dirname(__file__)
+    fileName = dt.now().strftime(fileNameDTFormat) + "_HL_Performance.txt"
+    print("Writing performance stats...")
+    filePath = os.path.join(currentDir, 'Data_Dumps', fileName)
+    outputFile = open(filePath, 'w')
+    outputFile.write("TimeStamp\tCPULoad\tDedicatedMemory\tDedicatedMemoryUsed\t" +
+                    "SystemMemory\tSystemMemoryUsed\tEnginesUtilization\n")
+    while (True):
+        try:
+            logPerformanceStats(outputFile)
+        except KeyboardInterrupt:
+            print("Coolio! Exiting program...")
+            sys.exit()
+
+if __name__ == '__main__':
+    writePerformanceStats()
