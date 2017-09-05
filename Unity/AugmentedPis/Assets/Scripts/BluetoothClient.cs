56858a134ea280f3134a455d9d82701503e6b93e
dchege711 <d.chege711@gmail.com>
2017-09-05 12:25:01 -0400

    Modularize MongoDB client.

---
+++ Unity/AugmentedPis/Assets/Scripts/BluetoothClient.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.IO.Ports;
+using System.Threading;
+
+/// <summary>
+/// Handles bluetooth data.
+/// Adapted from http://justkiel.com/wordpress/?p=1335.
+/// </summary>
+public class BluetoothClient : MonoBehaviour {
+
+    // First activate the serial ports. Use Settings --> Devices --> 
+    // Bluetooth & Other Devices --> More Bluetooth Options --> COM Ports
+
+    private SerialPort serialPort = null;
+    String portName = "COM3";   
+    int baudRate = 115200;      
+    int readTimeOut = 100;      
+    int bufferSize = 32;    
+
+    bool programActive = true;
+    Thread thread;
+
+    /// <summary>
+    /// Initialize resources needed for the program.
+    /// 1) Setup the bluetooth serial port and start the thread that handles msgs.
+    /// </summary>
+    void Start() {
+
+        try {
+            // Set the properties of the serial port.
+            serialPort = new SerialPort();
+            serialPort.PortName = portName;
+            serialPort.BaudRate = baudRate; 
+            serialPort.ReadTimeout = readTimeOut; 
+            serialPort.Open();
+            Debug.Log("Opened port " + serialPort.PortName);
+        }
+        catch (Exception e) {
+            Debug.Log(e.Message);
+        }
+
+        // Execute a thread to manage the incoming BT data.
+        // Don't read data in the update() method since reads block the program.
+        thread = new Thread(ProcessData);
+        thread.Start();
+    }
+
+    /// <summary>
+    /// Process the incoming BT data on the virtual serial port.
+    /// </summary>
+    void ProcessData() {
+        Byte[] buffer = new Byte[bufferSize];
+        int bytesRead = 0;
+        Debug.Log("Thread started");
+
+        while (programActive) {
+            try {
+                // Attempt to read data from the BT device
+                // - will throw an exception if no data is received within the timeout period
+                bytesRead = serialPort.Read(buffer, 0, bufferSize);
+
+                // Use the appropriate SerialPort read method for your BT device e.g. ReadLine(..) for newline terminated packets 
+
+                if (bytesRead > 0)
+                {
+                    Debug.Log("Received data. Let me print it!");
+                    Debug.Log(buffer);
+                }
+            }
+            catch (TimeoutException)
+            {
+                // Do nothing, the loop will be reset
+            }
+        }
+        Debug.Log("Thread stopped");
+    }
+
+    /// <summary>
+    /// Update this instance.
+    /// </summary>
+
+
+    void Update()
+    {
+    }
+
+    /// <summary>
+    /// On program exit.
+    /// </summary>
+
+
+    public void OnDisable()
+    {
+        programActive = false;
+
+        if (serialPort != null && serialPort.IsOpen)
+            serialPort.Close();
+    }
+}
--- Unity/AugmentedPis/Assets/Scripts/DBClient.cs
+++ Unity/AugmentedPis/Assets/Scripts/DBClient.cs
@@ -9,29 +9,55 @@ using MongoDB.Bson;
 
 // The syntax for v1.11.0 differs considerably from the current v2.4.4
 
+/// <summary>
+/// Handles connections, reads and writes to MongoDB.
+/// Currently buggy: Complains of an ssl connection.
+/// </summary>
 public class DBClient : MonoBehaviour {
+
+    // Declare variables that need to be accessed by different methods.
+    MongoServer server;
+
     void Start() {
-        string uri = "mongodb://ar-projects:sCQWaRWCzoS01RbIUNAL05dj0VlO9acJficROoIywMwm3DYRexg4e1J6IZwJmuJVvrVpfhD05QaCPWcsRlptjw==@ar-projects.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
+        string uri = "mongodb://ar-projects:sCQWaRWCzoS01RbIUNAL05dj0VlO9acJficROoIywMwm3DYRexg4e1J6IZwJmuJVvrVpfhD05QaCPWcsRlptjw==@ar-projects.documents.azure.com:10255/?ssl=true";
         // string uri = "mongodb://localhost";
 
         MongoClient client = new MongoClient(new MongoUrl(uri));
+        server = client.GetServer();
+        server.Connect();  
+    }
 
-        var server = client.GetServer();
-        server.Connect();
+    void TestRun() {
         var db = server.GetDatabase("Test");
-        MongoCollection<BsonDocument> collection = db.GetCollection<BsonDocument>("testCol");
+        var collection = db.GetCollection<BsonDocument>("testCol");
         BsonDocument document = new BsonDocument {
-            { "testX", "This is test #2." },
+            { "testX", "This is test #3." },
             { "testY", "I hope it makes it to Azure..." }
         };
-        collection.Insert(document);
+        var result = InsertDoc(collection, document);
+        Debug.Log(result);
+    }
 
-        // Query the database
-        var query = MongoDB.Driver.Builders.Query.EQ("testX", "This is a test.");
-        var fetchedDocs = collection.Find(query);
-        foreach (BsonDocument doc in fetchedDocs) {
-            Console.WriteLine(doc);
-        }
-        
+    /// <summary>
+    /// Find the documents on a collection that match a key-value combination.
+    /// </summary>
+    /// <param name="collection">A MongoCollection object containing Bson Documents.</param>
+    /// <param name="key">A string representing the field in the document.</param>
+    /// <param name="value">The value that should be found at the key's location.</param>
+    /// <returns> A MongoCursor that can be used to iterate through matching documents.</returns>
+    MongoCursor QueryCollection(MongoCollection<BsonDocument> collection, string key, string value) {
+        var query = MongoDB.Driver.Builders.Query.EQ(key, value);
+        MongoCursor fetchedDocs = collection.Find(query);
+        return fetchedDocs;
+    }
+
+    /// <summary>
+    /// Insert a BsonDocument into the specified collection.
+    /// </summary>
+    /// <param name="collection">A MongoCollection object containing Bson Documents.</param>
+    /// <param name="doc">The Bson Document that needs to be added to the database.</param>
+    /// <returns>A WriteConcernResult that contains information about the write.</returns>
+    WriteConcernResult InsertDoc(MongoCollection<BsonDocument> collection, BsonDocument doc) {
+        return collection.Insert(doc);
     }
 }
